c32365a056bd4a57bc06d8e4185711e2518da46f
Ogwara Precious <ogwaraprecious8@gmail.com>
2025-09-16 08:04:41 -0700

    done

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -20,6 +20,12 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+  "extends": "next/core-web-vitals",
+  "rules": {
+    "@typescript-eslint/no-explicit-any": "off"
+  }
+}
 ];
 
 export default eslintConfig;
